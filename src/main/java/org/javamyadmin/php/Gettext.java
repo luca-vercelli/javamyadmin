e253ccc5b3e3eedb6ecf171e896014b746e50719
Luca Vercelli <luca.vercelli.to@gmail.com>
2019-11-26 08:59:35 +0100

    gettext

--- src/main/java/org/javamyadmin/php/Gettext.java
+++ src/main/java/org/javamyadmin/php/Gettext.java
@@ -6,10 +6,13 @@ import java.util.ResourceBundle;
 public class Gettext {
 	
 	//see https://www.gnu.org/software/gettext/manual/html_node/Java.html
-	private static ResourceBundle myResources = ResourceBundle.getBundle("global");
+	private static ResourceBundle myResources = ResourceBundle.getBundle("translations");
 
 	public static String __(String s) {
 		try {
+			
+			// FIXME
+			
 			return myResources.getString(s);
 		} catch(MissingResourceException exc) {
 			return s;
