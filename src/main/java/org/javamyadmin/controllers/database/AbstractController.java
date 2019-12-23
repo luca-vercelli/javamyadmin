ae4b61f3908f83e060d8481b491881273a357866
Luca Vercelli <luca.vercelli.to@gmail.com>
2019-12-23 17:30:25 +0100

    structure controller v0

---
+++ src/main/java/org/javamyadmin/controllers/database/AbstractController.java
@@ -0,0 +1,31 @@
+package org.javamyadmin.controllers.database;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.javamyadmin.helpers.Response;
+import org.javamyadmin.php.Globals;
+import org.javamyadmin.php.Php.SessionMap;
+
+public abstract class AbstractController  extends org.javamyadmin.controllers.AbstractController {
+
+	private static final long serialVersionUID = -1;
+
+	@Override
+	final protected void doGet(HttpServletRequest request, HttpServletResponse response, Response pmaResponse,
+			SessionMap $_SESSION, Globals GLOBALS) throws ServletException, IOException {
+
+		Connection connection =  null; //TODO
+		
+		doGet(request, response, pmaResponse, $_SESSION, GLOBALS, connection);
+		
+	}
+
+	abstract protected void doGet(HttpServletRequest request, HttpServletResponse response, Response pmaResponse,
+			SessionMap $_SESSION, Globals GLOBALS, Connection $db) throws ServletException, IOException;
+	
+}
---
+++ src/main/java/org/javamyadmin/controllers/database/StructureController.java
@@ -0,0 +1,71 @@
+package org.javamyadmin.controllers.database;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.javamyadmin.helpers.Response;
+import org.javamyadmin.php.Globals;
+import org.javamyadmin.php.Php.SessionMap;
+
+/**
+ * Handles database structure logic
+ *
+ * @package PhpMyAdmin\Controllers
+ */
+public class StructureController extends AbstractController {
+
+	private static final long serialVersionUID = 8146583993048177980L;
+
+    /**
+     * @var int Number of tables
+     */
+    protected int $numTables;
+
+    /**
+     * @var int Current position in the list
+     */
+    protected int $position;
+
+    /**
+     * @var bool DB is information_schema
+     */
+    protected boolean $dbIsSystemSchema;
+
+    /**
+     * @var int Number of tables
+     */
+    protected int $totalNumTables;
+
+    /**
+     * @var array Tables in the database
+     */
+    protected List<?> $tables; //TODO
+
+    /**
+     * @var bool whether stats show or not
+     */
+    protected boolean $isShowStats;
+
+    /**
+     * @var Relation
+     */
+    //private Relation $relation; //TODO ?
+
+    /**
+     * @var Replication
+     */
+    //private Replication $replication; //TODO ?
+    
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response, Response pmaResponse,
+			SessionMap $_SESSION, Globals GLOBALS, Connection $db) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
