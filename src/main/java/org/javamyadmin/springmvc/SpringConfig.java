3a161f35cbee52e230e8c2dcd3a92a6555e49c27
Luca Vercelli <luca.vercelli.to@gmail.com>
2019-12-24 17:05:18 +0100

    static res

--- src/main/java/org/javamyadmin/springmvc/SpringConfig.java
+++ src/main/java/org/javamyadmin/springmvc/SpringConfig.java
@@ -15,12 +15,29 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = { "org.javamyadmin.controllers" })
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+	    	.addResourceHandler("/favicon.ico")
+	    	.addResourceLocations("/favicon.ico");
+        registry
+        	//.addResourceHandler("/js{pattern:/(?!whitelist|messages)([a-z0-9]+)\\.js.*}")
+			//.addResourceHandler("/js/**/{pattern:([a-z0-9]+)}.js")
+        	.addResourceHandler("/js/**")
+    		.addResourceLocations("/js/");
+        registry
+        	.addResourceHandler("/themes/**")
+        	.addResourceLocations("/themes/"); 
+    }
+    
 	@Bean
 	@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 	public Globals getGLOBALS() {
