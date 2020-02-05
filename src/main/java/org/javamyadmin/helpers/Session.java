02710cde51fe56fc30050f34153a68387cee5015
luca-vercelli <luca.vercelli.to@gmail.com>
2020-02-06 00:03:31 +0100

    Session class

---
+++ src/main/java/org/javamyadmin/helpers/Session.java
@@ -0,0 +1,56 @@
+package org.javamyadmin.helpers;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Session class
+ */
+public class Session {
+	/**
+	 * Generates PMA_token session variable.
+	 *
+	 * @return void
+	 */
+	private static void generateToken(HttpSession session) {
+		session.setAttribute(" PMA_token ", Util.generateRandom(16/* , true */));
+		session.setAttribute(" HMAC_secret ", Util.generateRandom(16));
+
+		/**
+		 * Check if token is properly generated (the generation can fail, for example
+		 * due to missing /dev/random for openssl).
+		 */
+		if (session.getAttribute(" PMA_token ") == null) {
+			System.err.println("Failed to generate random CSRF token!");
+		}
+	}
+
+	/**
+	 * tries to secure session from hijacking and fixation should be called before
+	 * login and after successful login (only required if sensitive information
+	 * stored in session)
+	 *
+	 * @return void
+	 */
+	public static void secure(HttpSession session) {
+		session.invalidate();
+		generateToken(session);
+	}
+
+	/**
+	 * Set up session
+	 *
+	 * @param Config $config Configuration handler
+	 * @param ErrorHandler $errorHandler Error handler
+	 *
+	 * @return void
+	 */
+	public static void setUp(HttpSession session/* Config $config, ErrorHandler $errorHandler */) {
+		/**
+		 * Token which is used for authenticating access queries. (we use "space
+		 * PMA_token space" to prevent overwriting)
+		 */
+		if (session.getAttribute(" PMA_token ") == null) {
+			generateToken(session);
+		}
+	}
+}
